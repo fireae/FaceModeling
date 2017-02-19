25907cef891d923cf32595167faf8aff68840844
Fanziapril <wufanzi412@gmail.com>
2017-02-19 22:38:35 +0800

    Source

---
+++ source/test/face_alignment.m
@@ -0,0 +1,39 @@
+function shape = face_alignment( BFMmodel,keypoints,BoundIdxSet,...
+    LearnedCascadedModel, Data, img, gt_trans, options,idata,msz )
+
+
+
+%% setup the fixed parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+nData = length(Data);
+n_init_randoms_test = options.n_init_randoms_test;
+paraDim  = options.paraSize;
+
+aligned_shape = zeros(n_init_randoms_test,paraDim);
+
+%% randomize which shape is used for initial position
+rIdx = randi([1,nData],n_init_randoms_test);
+
+%% iterations of n initial points %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for ir = 1 : n_init_randoms_test
+    
+    %% get random positions and inital shape indexs
+    idx    = rIdx(ir);
+    init_para = Data(idx).para; %% get randomly shape from others
+    init_trans = gt_trans;
+    
+    
+    %% detect landmarks using cascaded regression
+    aligned_shape(ir,:) = cascaded_regress( BFMmodel,keypoints,BoundIdxSet, ...
+        LearnedCascadedModel, img, init_para, init_trans, options,idata,msz );
+    
+    
+    
+end
+
+if n_init_randoms_test == 1
+    shape = aligned_shape;
+else
+    shape =aligned_shape;
+end
+
+end
